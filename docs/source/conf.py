b0e55bc175d8c8be6ff60849ea6464b070894e05
Piotr Bakalarski <piotrb5e3@gmail.com>
2018-06-10 22:09:28 +0200

    Use sphinx_rtd_theme when building for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -77,7 +77,10 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+if 'READTHEDOCS' in os.environ:
+    html_theme = 'sphinx_rtd_theme'
+else:
+    html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
