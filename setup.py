e536faa2d81ff1995d0bb25a3e9ae689ca03d8cf
Piotr Bakalarski <piotrb5e3@gmail.com>
2019-04-22 22:47:01 +0200

    Deploy to test pypi from every devel commit

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import os
 import re
 import codecs
 from setuptools import setup, find_packages
+from pygit2 import Repository
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -12,12 +13,23 @@ def read(*parts):
         return fp.read()
 
 
-def get_version():
+def get_version_string():
     try:
         s = read('sanic_validation', '__init__.py')
         return re.findall(r"^__version__ = '([^']+)'\r?$", s, re.M)[0]
     except IndexError:
         raise RuntimeError('Unable to determine version.')
+
+
+def get_version():
+    repo = Repository('.')
+    branch_name = repo.head.shorthand
+    commit_hash = repo.head.hex
+    version_suffix = '-devel-' + commit_hash if branch_name != 'master' else ''
+
+    version_string = get_version_string()
+
+    return version_string + version_suffix
 
 
 setup(
@@ -31,7 +43,7 @@ setup(
     author_email='piotrb5e3@gmail.com',
     license='GPLv3',
     install_requires=['sanic>=0.6.0', 'cerberus'],
-    setup_requires=['pytest-runner', 'pytest-flake8'],
+    setup_requires=['pytest-runner', 'pytest-flake8', 'pygit2'],
     tests_require=['pytest', 'aiohttp', 'flake8'],
     packages=find_packages(),
     classifiers=[
